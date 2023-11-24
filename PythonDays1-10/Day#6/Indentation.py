7bd951c629f8bf0b601d46d9a5132dbfd6aa0ff0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-24 14:46:39 -0700

    Importance of spaces in Python and guideline

---
+++ PythonDays1-10/Day#6/Indentation.py
@@ -0,0 +1,20 @@
+
+# MARK: - Indentation and the scope of it
+
+# Example #1:
+
+# def my_function():
+#   print("Hello")
+#   print("World")
+
+# Example #2:
+
+# def my_function():
+#   print("Hello")
+# print("World")
+
+# MARK: - Breakdown
+
+# The small indentation difference from example 1 to example 2 changes the printed output to the console.
+# The 2nd example only has one print statement in the function 
+
---
+++ PythonDays1-10/Day#6/IndentationExample1.py
@@ -0,0 +1,15 @@
+
+
+sky = "clear"
+
+def my_function():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+
+my_function()
+
+print("world")
+
